ad4113f6732e3e08343f1ec6d9deb812d563fc9f
Sanghyeop Lee <d0003@naver.com>
2023-12-14 00:29:35 +0900

    feat: 회원가입, 로그인

--- config/urls.py
+++ config/urls.py
@@ -17,14 +17,15 @@ Including another URLconf
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 from .views import Main, UploadFeed
 
 urlpatterns = [
     # path('admin/', admin.site.urls),
-    path('', Main.as_view()),
+    path('', Main.as_view(), name='main'),
     path('content/upload', UploadFeed.as_view()),
+    path('user/', include('user.urls'))
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
